5cb412885076a4cfeee9f8fe6d3fff0077ea4a4a
Noah Taylor <Noah.j.taylor@wsu.edu>
2018-10-10 12:36:02 -0700

    initial commit

---
+++ Spreadsheet/Spreedsheet/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spreedsheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //dataGridView1.ColumnCount = 26;
+            //dataGridView1.ColumnHeadersVisible = true;
+            //dataGridView1.Columns[0].Name = ("A");
+            dataGridView1.Columns.Add("columnA", "A");
+            dataGridView1.Columns.Add("columnA", "B");
+            dataGridView1.Columns.Add("columnA", "C");
+            dataGridView1.Columns.Add("columnA", "D");
+            dataGridView1.Columns.Add("columnA", "E");
+            dataGridView1.Columns.Add("columnA", "F");
+            dataGridView1.Columns.Add("columnA", "G");
+            dataGridView1.Columns.Add("columnA", "H");
+            dataGridView1.Columns.Add("columnA", "I");
+            dataGridView1.Columns.Add("columnA", "J");
+            dataGridView1.Columns.Add("columnA", "K");
+            dataGridView1.Columns.Add("columnA", "L");
+            dataGridView1.Columns.Add("columnA", "M");
+            dataGridView1.Columns.Add("columnA", "N");
+            dataGridView1.Columns.Add("columnA", "O");
+            dataGridView1.Columns.Add("columnA", "P");
+            dataGridView1.Columns.Add("columnA", "Q");
+            dataGridView1.Columns.Add("columnA", "R");
+            dataGridView1.Columns.Add("columnA", "S");
+            dataGridView1.Columns.Add("columnA", "T");
+            dataGridView1.Columns.Add("columnA", "U");
+            dataGridView1.Columns.Add("columnA", "V");
+            dataGridView1.Columns.Add("columnA", "W");
+            dataGridView1.Columns.Add("columnA", "X");
+            dataGridView1.Columns.Add("columnA", "Y");
+            dataGridView1.Columns.Add("columnA", "Z");
+        }
+
+        
+    }
+}
---
+++ Spreadsheet/SpreedsheetEngine/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SpreedsheetEngine
+{
+    public abstract class Cell : INotifyPropertyChanged
+    {
+        protected string text;
+        protected string val;
+
+        string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                if (text != value)
+                {
+                    text = value;
+                    OnPropertyChanged("Text");
+                }
+            }
+        }
+
+        string Value
+        {
+            get
+            {
+                return val;
+            }
+
+            set
+            {
+                val = Value;
+            }
+        }
+
+        protected int RowIndex { get; set; }
+        protected int ColumnIndex { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected void OnPropertyChanged(string text)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(text));
+            }
+        }
+
+        protected Cell(int newRowIndex, int newColumnIndex, string newText)
+        {
+            this.RowIndex = newRowIndex;
+            this.ColumnIndex = newColumnIndex;
+            this.text = newText;
+        }
+
+    }
+
+    public class Spreadsheet : INotifyPropertyChanged
+    {
+        private class SpreadSheetCell : Cell
+        {
+            public SpreadSheetCell(int newRowIndex, int newColumnIndex, string newText) : base(newRowIndex, newColumnIndex, newText)
+            {
+                /*
+                text = newText;
+                RowIndex = newRowIndex;
+                ColumnIndex = newColumnIndex;
+                */
+            }
+        }
+
+        private SpreadSheetCell[,] sheetCells;
+        private int columnCount;
+        private int rowCount;
+
+        public int ColumnCount { get { return columnCount; } }
+        public int RowCount { get { return rowCount; } }
+
+
+        public Spreadsheet(int rows, int columns)
+        {
+            sheetCells = new SpreadSheetCell[rows, columns];
+            rowCount = rows;
+            columnCount = columns;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    sheetCells[i, j] = new SpreadSheetCell(i, j, "");
+                }
+            }
+
+        }
+
+        private SpreadSheetCell getCell(int row, int column)
+        {
+            return sheetCells[row, column];
+        }
+
+        public event PropertyChangedEventHandler CellPropertyChanged;
+
+
+        protected void OnPropertyChanged(string text)
+        {
+            PropertyChangedEventHandler handler = CellPropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(text));
+            }
+        }
+    }
+
+}
