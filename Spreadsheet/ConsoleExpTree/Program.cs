bd63d0f8934f9b32898f6450869e15a4d05d897d
ntaylor <noah.j.taylor>
2018-10-26 10:35:51 -0700

    Fixed date (I am not from the future)

--- Spreadsheet/ConsoleExpTree/Program.cs
+++ Spreadsheet/ConsoleExpTree/Program.cs
@@ -52,7 +52,6 @@ namespace ConsoleExpTree
                     case "3":
                         Console.WriteLine(expTree.Eval());
                         break;
-
                     default:
                         break;
                 }
